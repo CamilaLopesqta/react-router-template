cb47ef2d36742bd899d298f88d7ba27eed44dfc6
Camila Lopes <camilalopesqta@gmail.com>
2024-03-25 20:51:10 -0300

    Exercícios refatorados de React Router

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,20 +3,16 @@ import HomePage from "../pages/HomePage";
 import ProfilePage from "../pages/ProfilePage";
 import ErroPage from "../pages/ErroPage";
 
-
 function Router() {
-
-    return (
-
-        <BrowserRouter>
-            <Routes>
-                <Route index path={"/"} element={<HomePage />} />
-                <Route path={"/profile/:name"} element={<ProfilePage />} />
-                <Route path={"*"} element={<ErroPage/>} />
-            </Routes>
-        </BrowserRouter>
-    )
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path={"/"} element={<HomePage />} />
+        <Route path={"/profile/:name"} element={<ProfilePage />} />
+        <Route path={"*"} element={<ErroPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default Router
+export default Router;
